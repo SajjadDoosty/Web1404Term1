23df1f2914f37334a5efe475b429a22b875e97b2
SajjadDoosty <sajjad.doosty.83@gmail.com>
2025-10-14 16:49:51 +0330

    add: 01 F

---
+++ 1404-06-22/01_F.js
@@ -0,0 +1,13 @@
+const inputs = process.argv.slice(2);
+
+const a = Number(inputs[0])
+const b = Number(inputs[1])
+
+let result
+
+const obj = {
+    numberA: a,
+    numberB: b,
+}
+
+console.log(obj);
